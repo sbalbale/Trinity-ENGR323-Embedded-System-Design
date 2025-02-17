42f894c0f551419ba28c5f882634acbf6837a458
Sean Balbale <sean.balbale@gmail.com>
2025-02-17 15:26:08 -0500

    added stopwatch and countup

--- Lab1/Code/count up/stopwatch.asm
+++ Lab1/Code/count up/stopwatch.asm
@@ -10,7 +10,7 @@ MAIN:       MOV     SP, #30h
 
             ; Timer 0 setup for 5ms
             MOV     TH0, #0ECh     
-            MOV     TL0, #078h
+            MOV     TL0, #078h     
 
             ; Initialize registers for time values
             MOV     R0, #00h      ; 1 second digit
@@ -23,78 +23,69 @@ MAIN:       MOV     SP, #30h
             ; Enable interrupts
             SETB    ET0            
             SETB    EA             
-            SETB    TR0       
+            SETB    TR0            
 
+MainLoop:   SJMP    MainLoop       
 
-; MSECDelay:  MOV     R7, #0C7h    ; 199 * 1us = 1ms
-; BackA:      DEC    R7
-;             NOP
-;             NOP
-;             CJNE   R7, #000h, BackA
-;             RET
-     
-
-MainLoop:   ; Check if 1 second has elapsed
-            MOV     A, R5
-            CJNE    A, #0C8h, MainLoop  ; Wait for 200 * 5ms
-            MOV     R5, #00h      ; Reset counter
-
-            ; Update seconds
-            INC     R0            ; Increment ones second
-            MOV     A, R0
-            CJNE    A, #0Ah, MainLoop  
-            MOV     R0, #00h      ; Reset ones second
-            
-            ; Update tens seconds
-            INC     R1            ; Increment tens seconds
-            MOV     A, R1
-            CJNE    A, #06h, MainLoop  
-            MOV     R1, #00h      ; Reset tens seconds
-            
-            ; Update minutes
-            INC     R2            ; Increment minutes
-            MOV     A, R2
-            CJNE    A, #0Ah, MainLoop  
-            MOV     R2, #00h      ; Reset minutes
-            
-            ; Update tens minutes
-            INC     R3            ; Increment tens minutes
-            MOV     A, R3
-            CJNE    A, #06h, MainLoop  
-            MOV     R3, #00h      ; Reset tens minutes
-            
-            SJMP    MainLoop      
-
+;this loop takes aproximatly 70 machine cycles. Timer value might need to be adjusted to  0xECh, 0xBDh.
+; default value is 0xECh, 0x77h
 Timer0_ISR: PUSH    ACC            
             PUSH    PSW
 
-            ; Reload timer for next 5ms
+            ; Reload timer
             CLR     TR0            
             MOV     TH0, #0ECh     
-            MOV     TL0, #078h     
+            MOV     TL0, #08Bh     
             CLR     TF0            
             SETB    TR0            
 
-            ; Increment 5ms counter
-            INC     R5            ; For main loop timing
+            ; Update time counter
+            INC     R5             ; Increment ms counter
+            MOV     A, R5
+            CJNE    A, #0C8h, Display    ; 200 * 5ms = 1 second
+            MOV     R5, #00h       ; Reset ms counter
 
-            ; Display multiplexing
+            ; Update seconds
+            INC     R0             ; Increment ones second
+            MOV     A, R0
+            CJNE    A, #0Ah, Display    ; If not 10
+            MOV     R0, #00h       ; Reset ones second
+            
+            ; Update tens seconds
+            INC     R1             ; Increment tens seconds
+            MOV     A, R1
+            CJNE    A, #06h, Display    ; If not 60 seconds
+            MOV     R1, #00h       ; Reset tens seconds
+            
+            ; Update minutes
+            INC     R2             ; Increment minutes
+            MOV     A, R2
+            CJNE    A, #0Ah, Display    ; If not 10 minutes
+            MOV     R2, #00h       ; Reset minutes
+            
+            ; Update tens minutes
+            INC     R3             ; Increment tens minutes
+            MOV     A, R3
+            CJNE    A, #06h, Display    ; If not 60 minutes
+            MOV     R3, #00h       ; Reset tens minutes
+
+Display:    ; Get current position and display digit
             MOV     A, R4          
             CJNE    A, #00h, Try_Pos1
-            MOV     A, R3          ; Display 10 minutes
+            MOV     A, R3          ; Get 10 minutes
             SJMP    Output_Digit
 
 Try_Pos1:   CJNE    A, #01h, Try_Pos2
-            MOV     A, R2          ; Display 1 minute
+            MOV     A, R2          ; Get 1 minute
             ORL     A, #10h        
             SJMP    Output_Digit
 
 Try_Pos2:   CJNE    A, #02h, Try_Pos3
-            MOV     A, R1          ; Display 10 seconds
+            MOV     A, R1          ; Get 10 seconds
             ORL     A, #20h        
             SJMP    Output_Digit
 
-Try_Pos3:   MOV     A, R0          ; Display 1 second
+Try_Pos3:   MOV     A, R0          ; Get 1 second
             ORL     A, #30h        
 
 Output_Digit:
